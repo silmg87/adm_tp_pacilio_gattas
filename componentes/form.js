a40c488809206cad422d51fbddddce35a5ee35d2
julian-pacilio <julian.pacilio.info@gmail.com>
2022-10-07 20:51:50 -0300

    Componentes agregados

---
+++ componentes/form.js
@@ -0,0 +1,16 @@
+Vue.component('form-personal', {
+    data: () => {
+        return {
+
+        }
+    },
+
+    template: `
+    
+        <h1>Soy el form</h1>
+    `,
+
+    methods: {
+        
+    }
+});
---
+++ componentes/resultado.js
@@ -0,0 +1,16 @@
+Vue.component('resultado-yoga', {
+    data: () => {
+        return {
+
+        }
+    },
+
+    template: `
+    
+        <h1>Hola soy el resultado</h1>
+    `,
+
+    methods: {
+        
+    }
+});
